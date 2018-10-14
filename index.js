7e1a90e678f5138d5654f893fc43387751aece79
Caren Keyes <cejkeyes@gmail.com>
2018-10-13 20:20:35 -0700

    add favicon

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function handleButtons(){
-    console.log('handle buttons ran');
+    const email="cejkeyes@gmial.com";
+
     $('.email').click(e => {
         e.preventDefault();
         console.log('email clicked')
+        window.location.href="mailto:cejkeyes@gmail.com"
     });
     $('.github').click(e => {
         e.preventDefault();
-        console.log('github clicked')
         window.open('https://github.com/Carenelizabeth');
     });
     $('.linkedin').click(e => {
         e.preventDefault();
-        console.log('linkedin clicked')
         window.open('https://www.linkedin.com/in/caren-keyes-5595b130/');
     });
 }
